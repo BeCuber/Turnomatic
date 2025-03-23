4ed3ecee4acf4331a38424d9f088fd7fefcbaee8
felinibertomcfalso <136160356+BeCuber@users.noreply.github.com>
2025-03-23 19:10:26 +0100

    Refacotr. Navigation through menubar is now managed by MenuBarManager()

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,8 +1,10 @@
+import os
+
 from PyQt5.QtWidgets import QMainWindow, QCalendarWidget, QTableWidget, QTableWidgetItem, QStackedWidget, QAction
 from PyQt5.QtGui import QIcon
 from PyQt5 import uic
-import os
 from src.logic.volunteer_manager import VolunteerManager
+from src.ui.widgets.menubar import MenuBarManager
 
 class MainWindow(QMainWindow):
 
@@ -23,23 +25,16 @@ class MainWindow(QMainWindow):
         self.stacked_widget = self.findChild(QStackedWidget, "stackedWidget")
         self.calendar = self.findChild(QCalendarWidget, "calendarWidget")
         self.volunteer_table = self.findChild(QTableWidget, "volunteerTableWidget")
-
-        # Set calendar page as default
-        self.show_calendar()
-
-        # Define menubar options -->
-
-        # --> Calendar
-        self.menu_calendar = self.findChild(QAction, "actionCalendario")
-        self.menu_calendar.triggered.connect(self.show_calendar)
-
-        # --> Menu volunteers
-        self.menu_volunteers = self.findChild(QAction, "actionLista_voluntarios")
-        self.menu_volunteers.triggered.connect(self.show_volunteer_list)
         
+        # Config menu manager
+        self.menu_manager = MenuBarManager(self, self.stacked_widget)
+        # Set calendar page as default
+        self.menu_manager.show_calendar()
+
         
         # Default view on volunteer_table
         self.check_day()
+
 
         # Click day
         self.calendar.selectionChanged.connect(self.check_day)
@@ -69,13 +64,3 @@ class MainWindow(QMainWindow):
             self.volunteer_table.setItem(row_idx, 0, QTableWidgetItem(v[0]))  # Name
             self.volunteer_table.setItem(row_idx, 1, QTableWidgetItem(v[1]))  # Lastname_1
             self.volunteer_table.setItem(row_idx, 2, QTableWidgetItem("🚑" if v[3] else "")) # Driver
-
-    
-    def show_calendar(self):
-        """Change to calendar page"""
-        self.stacked_widget.setCurrentIndex(0) 
-
-    
-    def show_volunteer_list(self):
-        """Change to volunteer list page"""
-        self.stacked_widget.setCurrentIndex(1) # Change to page_2
--- src/ui/widgets/menubar.py
+++ src/ui/widgets/menubar.py
@@ -1,7 +1,26 @@
 from PyQt5.QtWidgets import QMainWindow, QAction
 
 
-class AssembliesComboBox():
+class MenuBarManager():
     
-    def __init__(self):
-        pass
+    def __init__(self, main_window: QMainWindow, stacked_widget):
+        """Inicializa el gestor del menú de la ventana principal."""
+        self.main_window = main_window
+        self.stacked_widget = stacked_widget
+        self.setup_menu_actions()
+
+    def setup_menu_actions(self):
+        """Configura las acciones del menú."""
+        self.menu_calendar = self.main_window.findChild(QAction, "actionCalendario")
+        self.menu_calendar.triggered.connect(self.show_calendar)
+
+        self.menu_volunteers = self.main_window.findChild(QAction, "actionLista_voluntarios")
+        self.menu_volunteers.triggered.connect(self.show_volunteer_list)
+
+    def show_calendar(self):
+        """Cambia a la página del calendario."""
+        self.stacked_widget.setCurrentIndex(0)
+
+    def show_volunteer_list(self):
+        """Cambia a la página de lista de voluntarios."""
+        self.stacked_widget.setCurrentIndex(1)
